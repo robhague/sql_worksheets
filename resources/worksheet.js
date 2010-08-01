fddab219865b437c10af914e705b933c0ec1f754
Rob Hague <rob-github@rho.org.uk>
2010-08-01 09:25:15 +0100

    Added text area resizing.

--- resources/worksheet.js
+++ resources/worksheet.js
@@ -16,11 +16,41 @@ function ajax_request(path, value, response) {
   xmlhttp.send(value);
 }
 
+/*
+ * Textarea autoresizer code adapted from: 
+ *   http://www.sitepoint.com/blogs/2009/07/29/build-auto-expanding-textarea-1/
+ */
+var resize_text_area = function() {
+		var hCheck = !($.browser.msie || $.browser.opera);
+
+		// resize a textarea
+		return function() {
+			// find content length and box width
+			var vlen = this.value.length, ewidth = this.offsetWidth;
+			if (vlen != this.valLength || ewidth != this.boxWidth) {
+
+				if (hCheck && (vlen < this.valLength || ewidth != this.boxWidth)) {
+            this.style.height = "0px";
+        }
+				var h = this.scrollHeight;
+
+				this.style.overflow = "hidden";
+				this.style.height = h + "px";
+
+				this.valLength = vlen;
+				this.boxWidth = ewidth;
+			}
+
+			return true;
+		};
+}();
+
+
 /**
  * Process a given block
  */
 function process_block(blockID) {
-    query = $('#block'+blockID+' > .query').val();
+    var query = $('#block'+blockID+' > .query').val();
     if (query[0] == '?') {
         // SQL query
         ajax_request('.', 'sql_query='+encodeURIComponent(query.substring(1)),
@@ -37,7 +67,9 @@ function process_block(blockID) {
 var add_query = function() {
     var nextID = 1;
     return function(afterSelector) {
-        $(afterSelector).after('<div class="sql_query" id="block'+nextID+'"><textarea class="query" onchange="process_block('+nextID+')"></textarea><div class="answer"></div></div>');
+        $(afterSelector).after('<div class="block" id="block'+nextID+'"><textarea class="query" rows="1" onchange="process_block('+nextID+')"></textarea><div class="answer"></div></div>');
+				$('#block'+nextID+' > .query').bind("keyup", resize_text_area)
+            .bind("focus", resize_text_area);
         $('#block'+nextID+' > .query').focus();
         nextID++;
     }
